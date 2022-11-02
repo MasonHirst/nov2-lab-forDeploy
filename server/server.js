a1258ac9963121797f247d85658c53fcddf1da5f
Mason Hirst <mason.j.hirst@gmail.com>
2022-11-02 15:16:38 -0600

    first init

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const {home} = require('./controllers/pageCtrl')
+
+app.get("/", home)
+
+
+const { PORT } = process.env
+
+
+
+
+app.listen(PORT, () => console.log('server running on port ', PORT))
